42598d0a6f18ca68c3ba15a6b85dac10b7ce34ac
Kadeesya <kadeesya2711@gmail.com>
2025-04-20 20:48:16 +0800

    user authentication

---
+++ routes/user_routes.py
@@ -0,0 +1,29 @@
+from flask import Blueprint, render_template, request
+
+user_bp = Blueprint('user',__name__)
+
+@user_bp.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@user_bp.route('/login')
+def login():
+    return render_template('login.html')
+
+@user_bp.route('/signup-user', methods=['POST'])
+def handle_signup():
+    name = request.form['name']
+    email = request.form['email']
+    password = request.form['password']
+    # You can save to a database here!
+    return f"Welcome {name}, you’ve successfully signed up with {email}!"
+
+@user_bp.route('/login-user', methods=['POST'])
+def handle_login():
+    name = request.form['name']
+    username = request.form['username']
+    password = request.form['password']
+    if username == 'user' and password == '1234':
+        return f"Welcome back {name}, Login successful!"
+    else:
+        return "Invalid Login credentials!"
